028953d6fb5224dc3274f8197aba934c0448b6f5
Victor Resende <victoresende19@gmail.com>
2024-11-07 02:42:44 -0300

    yt6

--- api/service/transcript_service.py
+++ api/service/transcript_service.py
@@ -1,11 +1,16 @@
-from langchain_community.document_loaders import YoutubeLoader
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import TextFormatter
+from youtube_transcript_api._errors import NoTranscriptFound, TranscriptsDisabled
 
 def get_transcription(video_url: str, languages=['en', 'pt']) -> str:
-    """
-    Fetches the transcription for a given YouTube video URL in the specified languages.
-    """
-    loader = YoutubeLoader.from_youtube_url(
-        video_url, 
-        add_video_info=False
-    )
-    return loader.load()
+    try:
+        video_id = video_url.split("v=")[-1]
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages)
+        formatter = TextFormatter()
+        return formatter.format_transcript(transcript)
+    except NoTranscriptFound:
+        return f"No transcript found for the requested languages: {languages}. Try another video."
+    except TranscriptsDisabled:
+        return "Transcripts are disabled for this video."
+    except Exception as e:
+        return f"An unexpected error occurred: {str(e)}"
