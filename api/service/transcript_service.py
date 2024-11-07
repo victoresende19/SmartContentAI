622e99166442a13a831987d8b7b7c9005b6b44c2
Victor Resende <victoresende19@gmail.com>
2024-11-07 02:33:38 -0300

    yt2

--- api/service/transcript_service.py
+++ api/service/transcript_service.py
@@ -5,7 +5,7 @@ def get_transcription(video_url: str, languages=['en', 'pt']) -> str:
     Fetches the transcription for a given YouTube video URL in the specified languages.
     """
     loader = YoutubeLoader.from_youtube_url(
-        "https://www.youtube.com/watch?v=QsYGlZkevEg", add_video_info=False
+        video_url, 
+        add_video_info=False
     )
-
     return loader.load()
